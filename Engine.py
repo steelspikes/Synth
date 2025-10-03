20b7c683e9d6153ee1889b14af74ea1a869cefb7
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2025-10-03 12:48:43 -0600

    Remove unused files

--- Engine.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-
-class Engine:
-    def __init__(self, sample_rate=44100):
-        self.modules = []
-        self.sample_rate = sample_rate
-
-    def play(self, sample):
-        sd.play(sample, self.sample_rate)
