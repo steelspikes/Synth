ce24dbc31b405be920eba2cd8c3aff68e516d1b8
Brandon Salvador Martínez López <50210332+steelspikes@users.noreply.github.com>
2026-01-18 19:18:44 -0600

    Antes de quitar LFO

--- parallelEvaluation.py
+++ parallelEvaluation.py
@@ -5,7 +5,7 @@ import numpy as np
 import librosa
 
 def evaluate_presets(data):
-    presets, target_C = data
+    presets, target_C, multi = data
     target_A, target_B, target_C = target_C
     synth = Synth(
         sample_rate=SAMPLE_RATE,
@@ -22,12 +22,14 @@ def evaluate_presets(data):
     # env_target = librosa.feature.rms(y=target_audio, frame_length=256)
     # env_loss = np.mean(np.abs(env_synth - env_target))
 
-    current_A, current_B, current_C = (
-        mel_spectrogram(audio, sr=SAMPLE_RATE),0,0
-        # mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=1024, hop_length=128, n_mels=64), 
-        # mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=2048, hop_length=256, n_mels=256),
-        # mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=4096, hop_length=512, n_mels=256)
-    )
+    if multi:
+        current_A, current_B, current_C = (
+            mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=1024, hop_length=128, n_mels=64), 
+            mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=2048, hop_length=512, n_mels=128),
+            mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=4096, hop_length=256, n_mels=256)
+        )
 
-    # return 0.2*MSE(current_A, target_A) + 0.4*MSE(current_B, target_B) + 0.4*MSE(current_C, target_C)
-    return MSE(current_A, target_A)
+        return 0.2*MSE(current_A, target_A) + 0.4*MSE(current_B, target_B) + 0.4*MSE(current_C, target_C)
+    else:
+        current_B = mel_spectrogram(audio, sr=SAMPLE_RATE, n_fft=2048, hop_length=512, n_mels=128)
+        return MSE(current_B, target_B)
